63db85397fbef47c0da8a6d16841bdc0392e9ad4
Dhayanne Espindola <dhayanneespindola@MacBook-Air-de-Dhayanne.local>
2025-06-15 13:21:55 -0300

    aaaaa

---
+++ funcoes.py
@@ -0,0 +1,10 @@
+def saudacao(nome, idade):
+    print(f"Olá {nome}! E eu tenho {idade} anos") 
+
+saudacao("Dhay", 20)
+saudacao("Dhay", 20)
+saudacao("Dhay", 20)
+saudacao("Dhay", 20)
+saudacao("Dhay", 20)
+saudacao("Dhay", 20)
+saudacao("Dhay", 20)
